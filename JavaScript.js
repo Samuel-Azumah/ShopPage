6a4dccd1f4c315f2bb58725f89e0dbc13d70bd25
Samuel-Azumah <72462228+Samuel-Azumah@users.noreply.github.com>
2020-10-10 02:03:44 -1200

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -16,4 +16,51 @@ function addItem(id, name, description, price, moreInfo) {
 
   $('#container').prepend(html);
 }
+$(document).ready(function(){
+  $('#container').on('click','.more-info-link', function(event){
+    event.preventDefault();
 
+    $(this).parent().find('.more-info').slideToggle('slow');
+    $(this)
+      .animate({ "opacity": 0.5, "margin-left": 10 }, 150)
+      .animate({ "opacity": 1.0, "margin-left": 0 }, 150);
+  });
+
+  $('#container').on('click','.item-remove',function(){
+    $(this).parent().remove();
+  });
+
+  $.ajax('data/item.json', {
+    dataType: 'json',
+    contentType: 'application/json',
+    cache: false
+  })
+  .done(function(response){
+    let items = response.items;
+    items.forEach(function(item){
+      addItem(item.id, item.name, item.description, item.price, item.moreInfo);
+    });
+  })
+  .fail(function(request, errorType, errorMessage){
+    console.log(errorMessage);
+  })
+  .always(function(){
+
+  });
+
+  $('#container').on('click','.item-add',function(){
+    let id = $(this).parent().data('id');
+
+    $.ajax('data/addToCart.json', {
+      type: 'post',
+      data: { id: id },
+      dataType: 'json',
+      contentType: 'application/json'
+    })
+    .done(function(response) {
+      if (response.message === 'success') {
+        let price = response.price;
+
+        cart += price;
+
+ 
